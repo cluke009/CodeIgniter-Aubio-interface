ab0dc0dd825c54761237ed42e3a87fec78f148f3
Geraldo Ramos <gramos1984@gmail.com>
2011-05-16 08:38:43 -0400

    clean up unused files

---
+++ application/views/header.php
@@ -0,0 +1,16 @@
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+
+<!-- SCRIPTS -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js" type="text/javascript"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/jquery-ui.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/js/jquery.fileinput.min.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/js/swfobject.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/js/jwplayer.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/js/script.js" type="text/javascript"></script>
+
+<!-- STYLES -->
+<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/themes/ui-lightness/jquery-ui.css" type="text/css"/>
+<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/grid.css"/>
+<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/fileinput.css"/>
+<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/style.css"/>
+
