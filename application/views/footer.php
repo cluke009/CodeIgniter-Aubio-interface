d661c2e4d569d4c54a2ea35c18763f83aa55c1a9
Geraldo Ramos <gramos1984@gmail.com>
2011-05-22 18:24:36 -0400

    Remove template-less views

--- application/views/footer.php
+++
@@ -1,6 +0,0 @@
-<div class="column grid_12 footer ">
-  Made with <a href="http://codeigniter.com/">CodeIgniter</a>
-  <a href="http://jqueryui.com/">Jquery UI</a> and
-  <a href="http://aubio.org/aubiocut.html">Aubiocut</a>
-</div>
-
