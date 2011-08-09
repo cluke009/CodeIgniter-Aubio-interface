4f80a1747affc2d00566c518488333a326c91499
Geraldo Ramos <gramos1984@gmail.com>
2011-08-09 02:59:42 -0400

    add waveform to mp3 player

--- application/views/beats_view.php
+++ application/views/beats_view.php
@@ -1,28 +1,3 @@
-<?php
-// http://www.codingforums.com/archive/index.php/t-169937.html
-function wavDur($file) {
-  $fp = fopen($file, 'r');
-  if (fread($fp,4) == "RIFF") {
-    fseek($fp, 20);
-    $rawheader = fread($fp, 16);
-    $header = unpack('vtype/vchannels/Vsamplerate/Vbytespersec/valignment/vbits',$rawheader);
-
-    $pos = ftell($fp);
-    while (fread($fp,4) != "data" && !feof($fp)) {
-      $pos++;
-      fseek($fp,$pos);
-    }
-    $rawheader = fread($fp, 4);
-    $data = unpack('Vdatasize',$rawheader);
-    $sec = $data['datasize']/$header['bytespersec'];
-    $minutes = intval(($sec / 60) % 60);
-    $seconds = intval($sec % 60);
-#    return str_pad($minutes,2,"0", STR_PAD_LEFT).":".str_pad($seconds,2,"0", STR_PAD_LEFT);
-    return $seconds;
-  }
-}
-
-?>
 <div id="tabs">
   <ul>
     <li><a href="#tabs-1">Uploaded Beats</a></li>
@@ -45,15 +20,15 @@ function wavDur($file) {
   <div class="list-row portlet <?php echo $class; ?>">
     <div class="portlet-header"><?php echo $value['title'];?> : <?php echo $timestamp ?></div>
     <div class="portlet-content">
+      <img src="<?php echo base_url() . str_replace('mp3','png',$value['file']);?>"/>
       <div id='mediaspace<?=$item?>'>This text will be replaced</div>
 
       <script type='text/javascript'>
         jwplayer('mediaspace<?=$item?>').setup({
           'flashplayer': '<?=base_url()?>assets/swf/player.swf',
           'file': '<?php echo base_url() . $value['file'];?>',
-          'duration': '<?php echo wavDur($value['file']);?>',
           'controlbar': 'bottom',
-          'width': '470',
+          'width': '500',
           'height': '24'
         });
       </script>
